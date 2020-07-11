1df466a7865d6439b7d1cb97ec2f890b8bd94f0a
msethi1980 <manishsethi.2004@gmail.com>
2020-07-11 21:37:32 +0530

    34sdjskd

---
+++ testjavaproject/src/com/javacodegeeks/EvenNumbers.java
@@ -0,0 +1,10 @@
+package com.javacodegeeks;
+
+public class EvenNumbers {
+
+	public static void printEvenNumber(int i) {
+		System.out.println("Even numbers upto " + i);
+		for (int x = 0; x < i; x = x + 2)
+			System.out.println(x);
+	}
+}
---
+++ testjavaproject/src/com/javacodegeeks/OddNumbers.java
@@ -0,0 +1,9 @@
+package com.javacodegeeks;
+
+public class OddNumbers {
+    public static void printOddNumber(int i) {
+        System.out.println("Odd numbers upto "+i);
+        for(int x=1;x<i;x=x+2)
+            System.out.println(x);
+    }
+}
