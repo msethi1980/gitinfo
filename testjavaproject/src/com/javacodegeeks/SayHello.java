e6ac7104e9653ac8827cd70fe3e8d23ddc644790
msethi1980 <manishsethi.2004@gmail.com>
2020-07-11 21:48:47 +0530

    dfdf

--- testjavaproject/src/com/javacodegeeks/SayHello.java
+++ testjavaproject/src/com/javacodegeeks/SayHello.java
@@ -2,8 +2,10 @@ package com.javacodegeeks;
  
 public class SayHello {
     public static void main(String[] args) {
-        System.out.println("Hello there!");
+    	System.out.println("Hello there!");
+        System.out.println("Printing even numbers");
         EvenNumbers.printEvenNumber(11);
+        System.out.println("Printing odd numbers");
         OddNumbers.printOddNumber(12);
     }
 }
